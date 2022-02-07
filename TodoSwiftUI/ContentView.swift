4d7c9bba666640db55596f00b9bf1c8146599978
Alpsu Dilbilir <alpsudilbilir@gmail.com>
2022-02-07 05:44:31 +0300

    Initial commit

---
+++ TodoSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TodoSwiftUI
+//
+//  Created by Alpsu Dilbilir on 7.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selected = "Yellow"
+    var body: some View {
+        VStack {
+           
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TodoSwiftUI/TodoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoSwiftUIApp.swift
+//  TodoSwiftUI
+//
+//  Created by Alpsu Dilbilir on 7.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
